09f686deb61b079dc877a8c76d244e09b1e841b8
Кормилицын Илья Сергеевич <ip311s11@csc.sibsutis.ru>
2025-05-30 23:56:36 +0700

    Изменнения App, index

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./scss/app.scss";
+import CountryCards from "./pages/CountryCards";
+
+function App() {
+  return (
+    <>
+      <CountryCards />
+    </>
+  );
+}
+
+export default App;
