3fc7466a313df7b1fafbb4b7ba2b3ddbcd4de5b6
Кормилицын Илья Сергеевич <ip311s11@csc.sibsutis.ru>
2025-05-31 16:52:42 +0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "./scss/app.scss";
 import CountryCards from "./pages/CountryCards";
+import Header from "./pages/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <CountryCards />
     </>
   );
