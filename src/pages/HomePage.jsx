e000c226950f368e92bfe89b789e1987890e803b
Кормилицын Илья Сергеевич <ip311s11@csc.sibsutis.ru>
2025-06-01 18:30:18 +0700

    добавил страничку Home

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import CountryCards from "../components/CountryCards";
+import Filters from "../components/Filters";
+
+function HomePage() {
+  return (
+    <>
+      <Filters />
+      <CountryCards />
+    </>
+  );
+}
+
+export default HomePage;
