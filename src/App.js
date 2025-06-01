ca9c2ae09289387929fcf10d3a1cf5b2b008e41d
Кормилицын Илья Сергеевич <ip311s11@csc.sibsutis.ru>
2025-06-01 18:32:58 +0700

    добаил i18n, фикс app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
-import CountryDetail from "./components/CountryDetail";
+import CountryDetail from "./components/CountryDetails";
 import "./scss/app.scss";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/country/:code" element={<CountryDetail />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
+import "./scss/app.scss";
+import countryReducer from "./features/countrySlice";
+import { countryApi } from "./services/countryApi";
+import "./i18n";
+
+const store = configureStore({
+  reducer: {
+    country: countryReducer,
+    [countryApi.reducerPath]: countryApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(countryApi.middleware),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
